5bcc718ed32846b98cc053c475450a5831239fc0
Charlie Lyons <charlietlyons@gmail.com>
2022-08-08 20:06:28 -0500

    Initial Commit, basic React webpack project setup

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Thing } from "./component/Thing";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Thing/>);
---
+++ src/component/Thing.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const Thing = () => {
+    return <>
+        Just some stuff!
+    </>
+}
+
